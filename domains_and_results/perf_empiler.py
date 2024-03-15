c874583472cf7d5b90345fccb980ae9fcb422fa6
AnthonyFavier <anthony.favier@laas.fr>
2024-03-15 11:42:25 +0100

    perf scenario 9+10

--- domains_and_results/perf_empiler.py
+++ domains_and_results/perf_empiler.py
@@ -1045,6 +1045,275 @@ def scenario_8():
         def set(self, agent_name, value):
             setattr(self, agent_name, value)
 
+def scenario_9():
+    CUBES = {
+        'b1',
+        'g1',
+        's1',
+        'p2',
+        'r1',
+        'b3',
+        'y1',
+        'w1',
+        'o1',
+        'b2',
+        'p1',
+        'r2',
+    }
+    class Cubes:
+
+        __slots__ = ()
+        for c in CUBES:
+            __slots__ += (''.join((c, '_on')), ''.join((c, '_below')) )
+
+        def __init__(self) -> None:
+            self.p2_on = {'R'}
+            self.b1_on = {'R'}
+            self.g1_on = {'b1'}
+            self.r1_on = {'R'}
+            self.s1_on = {'R'}
+
+            self.y1_on = {'C'}
+            self.o1_on = {'C'}
+            self.w1_on = {'C'}
+            self.b3_on = {'C'}
+
+            self.b2_on = {'H'}
+            self.p1_on = {'H'}
+            self.r2_on = {'H'}
+
+            self.computeBelow()
+
+        def computeBelow(self):
+            for c in CUBES:
+                setattr(self, ''.join([c, '_below']), None)
+            for c in CUBES:
+                c_on = getattr(self, ''.join([c, '_on']))
+                if len(c_on):
+                    for cube_on in c_on: break
+                    if cube_on not in {'R', 'C', 'H'}:
+                        setattr(self, ''.join([cube_on, '_below']), c)
+
+        def get_on(self, cube_name):
+            return getattr(self, ''.join([cube_name, '_on']))
+        def set_on(self, cube_name, value):
+            setattr(self, ''.join([cube_name, '_on']), value)
+        
+        def get_below(self, cube_name):
+            return getattr(self, ''.join([cube_name, '_below']))
+    class Colors:
+
+        __slots__ = CUBES
+        
+        def __init__(self) -> None:
+            for c in CUBES:
+                setattr(self, c, c[0])
+
+        def get(self, cube_name):
+            return getattr(self, cube_name)
+
+    LOCATIONS = {
+        'l1',
+        'l2',
+        'l3',
+        'l4',
+        'l5',
+        'l6',
+        'l7',
+        # 'l8',
+        'l9',
+        'l10',
+    }
+    class Solution:
+        __slots__ = ()
+        for l in LOCATIONS:
+            __slots__ += ( ''.join((l, '_color')), ''.join((l, '_on')) )
+
+        def __init__(self) -> None:
+            self.l1_color = 'r'
+            self.l1_on = {'table'}
+            self.l2_color = 'y'
+            self.l2_on = {'table'}
+            self.l3_color = 'p'
+            self.l3_on = {'l1','l2'}
+            self.l4_color = 'o'
+            self.l4_on = {'l3'}
+            self.l5_color = 's'
+            self.l5_on = {'l3','l10'}
+            self.l6_color = 'w'
+            self.l6_on = {'l4'}
+            self.l7_color = 'b'
+            self.l7_on = {'l5'}
+            # self.l8_color = 'p'
+            # self.l8_on = {'l6','l7'}
+            self.l9_color = 'b'
+            self.l9_on = {'table'}
+            self.l10_color = 'r'
+            self.l10_on = {'l9'}
+
+        def get_on(self, l):
+            return getattr(self, ''.join([l, '_on']))
+        def get_color(self, l):
+            return getattr(self, ''.join([l, '_color']))
+    class CurrentStack:
+
+        __slots__ = LOCATIONS
+
+        def __init__(self) -> None:
+            for l in LOCATIONS:
+                setattr(self, l, None)
+
+        def get(self, l):
+            return getattr(self, l)
+        def set(self, l, value):
+            setattr(self, l, value)
+
+    class Holding:
+        __slots__ = ('R', 'H')
+        def __init__(self) -> None:
+            self.R = None
+            self.H = None
+
+        def get(self, agent_name):
+            return getattr(self, agent_name)
+        def set(self, agent_name, value):
+            setattr(self, agent_name, value)
+
+def scenario_10():
+    CUBES = {
+        'b1',
+        'g1',
+        's1',
+        'p2',
+        'r1',
+        'b3',
+        'y1',
+        'w1',
+        'o1',
+        'b2',
+        'p1',
+        'r2',
+    }
+    class Cubes:
+
+        __slots__ = ()
+        for c in CUBES:
+            __slots__ += (''.join((c, '_on')), ''.join((c, '_below')) )
+
+        def __init__(self) -> None:
+            self.p2_on = {'R'}
+            self.b1_on = {'R'}
+            self.g1_on = {'b1'}
+            self.r1_on = {'R'}
+            self.s1_on = {'R'}
+
+            self.y1_on = {'C'}
+            self.o1_on = {'C'}
+            self.w1_on = {'C'}
+            self.b3_on = {'C'}
+
+            self.b2_on = {'H'}
+            self.p1_on = {'H'}
+            self.r2_on = {'H'}
+
+            self.computeBelow()
+
+        def computeBelow(self):
+            for c in CUBES:
+                setattr(self, ''.join([c, '_below']), None)
+            for c in CUBES:
+                c_on = getattr(self, ''.join([c, '_on']))
+                if len(c_on):
+                    for cube_on in c_on: break
+                    if cube_on not in {'R', 'C', 'H'}:
+                        setattr(self, ''.join([cube_on, '_below']), c)
+
+        def get_on(self, cube_name):
+            return getattr(self, ''.join([cube_name, '_on']))
+        def set_on(self, cube_name, value):
+            setattr(self, ''.join([cube_name, '_on']), value)
+        
+        def get_below(self, cube_name):
+            return getattr(self, ''.join([cube_name, '_below']))
+    class Colors:
+
+        __slots__ = CUBES
+        
+        def __init__(self) -> None:
+            for c in CUBES:
+                setattr(self, c, c[0])
+
+        def get(self, cube_name):
+            return getattr(self, cube_name)
+
+    LOCATIONS = {
+        'l1',
+        'l2',
+        'l3',
+        'l4',
+        'l5',
+        # 'l6',
+        'l7',
+        # 'l8',
+        'l9',
+        'l10',
+    }
+    class Solution:
+        __slots__ = ()
+        for l in LOCATIONS:
+            __slots__ += ( ''.join((l, '_color')), ''.join((l, '_on')) )
+
+        def __init__(self) -> None:
+            self.l1_color = 'r'
+            self.l1_on = {'table'}
+            self.l2_color = 'y'
+            self.l2_on = {'table'}
+            self.l3_color = 'p'
+            self.l3_on = {'l1','l2'}
+            self.l4_color = 'o'
+            self.l4_on = {'l3'}
+            self.l5_color = 's'
+            self.l5_on = {'l3','l10'}
+            # self.l6_color = 'w'
+            # self.l6_on = {'l4'}
+            self.l7_color = 'b'
+            self.l7_on = {'l5'}
+            # self.l8_color = 'p'
+            # self.l8_on = {'l6','l7'}
+            self.l9_color = 'b'
+            self.l9_on = {'table'}
+            self.l10_color = 'r'
+            self.l10_on = {'l9'}
+
+        def get_on(self, l):
+            return getattr(self, ''.join([l, '_on']))
+        def get_color(self, l):
+            return getattr(self, ''.join([l, '_color']))
+    class CurrentStack:
+
+        __slots__ = LOCATIONS
+
+        def __init__(self) -> None:
+            for l in LOCATIONS:
+                setattr(self, l, None)
+
+        def get(self, l):
+            return getattr(self, l)
+        def set(self, l, value):
+            setattr(self, l, value)
+
+    class Holding:
+        __slots__ = ('R', 'H')
+        def __init__(self) -> None:
+            self.R = None
+            self.H = None
+
+        def get(self, agent_name):
+            return getattr(self, agent_name)
+        def set(self, agent_name, value):
+            setattr(self, agent_name, value)
+
+
 
 if __name__ == "__main__":
     sys.setrecursionlimit(100000)
